3fa851af819937e6a0f12c9d23e0f215be9ca5a5
Akshi <akshigoel56@gmail.com>
2025-02-03 19:44:55 +0530

    Initial commit

---
+++ src/main/java/com/example/sprinbootrestapi/SprinbootrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootrestapiApplication.class, args);
+	}
+
+}
