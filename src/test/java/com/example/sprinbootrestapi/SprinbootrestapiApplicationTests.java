3dab852ab19bf50dbf23c048e96cfc7b84927c40
Akshi <akshigoel56@gmail.com>
2025-02-03 19:46:25 +0530

    Test Application

---
+++ src/test/java/com/example/sprinbootrestapi/SprinbootrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinbootrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
